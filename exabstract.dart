8daeda64193174b6414a7b9af12c517c7ccadf7b
1108Artittaya <3651051541108@rmutr.ac.th>
2024-09-27 12:16:41 +0700

    Add files via upload

---
+++ exabstract.dart
@@ -0,0 +1,52 @@
+//อาทิตยา ผงทอง 1108
+
+import 'dart:math';
+
+abstract class Shape {
+  void calculateArea();
+}
+
+abstract class Drawable {
+  void draw();
+}
+
+class circle implements Shape, Drawable {
+  double? radius;
+  circle(this.radius);
+
+  @override
+  void calculateArea() {
+    double Sum = pi * radius! * radius!;
+    print('Area of Circle : ${Sum.toStringAsFixed(2)}');
+  }
+
+  @override
+  void draw() {
+    print('Drawing Circle with radius: $radius');
+  }
+}
+
+class Rectangle implements Shape, Drawable {
+  double? width, height;
+  Rectangle(this.width, this.height);
+
+  @override
+  void calculateArea() {
+    double x = width! * height!;
+    print('Area of Circle: $x');
+  }
+
+  @override
+  void draw() {
+    print('Drawing Rectangle with width: $width and height: $height');
+  }
+}
+
+void main(List<String> args) {
+  var c = circle(5);
+  c.draw();
+  c.calculateArea();
+  var rectangle = Rectangle(10, 20);
+  rectangle.draw();
+  rectangle.calculateArea();
+}
